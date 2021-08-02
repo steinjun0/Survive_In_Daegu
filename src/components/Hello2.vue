2864e129d38c4e54f19af3a005f50d9c8f957ef9
steinjun0 <1142308@naver.com>
2021-08-02 16:34:58 +0900

    vue router 설정 및 vuex, vue-router  설치

--- src/components/Hello2.vue
+++ src/components/Hello2.vue
@@ -3,13 +3,12 @@
     <img src="http://vuejs.org/images/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ol>
-      <li><router-link to="/foo">Go to Foo</router-link></li>
+    <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ol>
+    </ul>
     <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
