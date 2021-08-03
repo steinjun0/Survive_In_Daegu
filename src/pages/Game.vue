b560cb6837e7e8561e117687930ab776f636abdb
crowcrow07 <skathd3402@gmail.com>
2021-08-03 23:06:48 +0900

    Game 글씨 중간에 추가

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -19,8 +19,18 @@
         </div>
       </v-col>
     </v-row>
+
     <v-row>
-      <v-col> </v-col>
+      <v-col>
+        <div
+          v-for="(text, tIndex) in storyText[paragraphIndex]"
+          :key="tIndex"
+          style="white-space: pre"
+          class="d-flex justify-center"
+        >
+          {{ text }}
+        </div>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -28,6 +38,25 @@
 <script>
 export default {
   name: "Game",
+  data() {
+    return {
+      storyText: [
+        [
+          "좀비와 외계인 침략이 생긴지 한달후,\n 집에 먹을것이 떨어진 당신은\n 집을 벗어나서 먹을것을 찾아야만 합니다.",
+        ],
+        ["당신은 여행을 떠나기전에", "을 챙겼습니다."],
+      ],
+      paragraphIndex: 0,
+    };
+  },
+  mounted() {},
+  methods: {
+    moveNextParagraph() {
+      if (this.paragraphIndex < 1) {
+        this.paragraphIndex += 1;
+      }
+    },
+  },
 };
 </script>
 
