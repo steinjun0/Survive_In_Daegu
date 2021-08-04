ed94de1d748a654ccf6a88ee98dedd09e228d215
crowcrow07 <skathd3402@gmail.com>
2021-08-04 18:39:37 +0900

    구구단 문제 결과 완성

--- src/components/Gugudan.vue
+++ src/components/Gugudan.vue
@@ -22,11 +22,69 @@
     <v-row>
       <v-col> </v-col>
     </v-row>
+    <v-row>
+      <v-col class="d-flex; flex-direction:column;" style="font-size: 40px">
+        {{ number }}곱하기 {{ number2 }}는?
+        <div style="width: 200px">
+          <v-text-field
+            v-model="answer"
+            :rules="nameRules"
+            :counter="10"
+            label="answer"
+            required
+          ></v-text-field>
+        </div>
+        <div>
+          <v-btn
+            type="submit"
+            style="margin-left: 48px; font-size: 24px"
+            @click="submitanswer"
+          >
+            입력!
+          </v-btn>
+        </div>
+        <div id="result" style="font-size: 25px">{{ result }}</div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Gugudan",
+  data() {
+    return {
+      number: Math.ceil(Math.random() * 9),
+      number2: Math.ceil(Math.random() * 9),
+      answer: "",
+      result: "",
+      nameRules: [
+        (v) => !!v || "정답을 적으세요!",
+        (v) => this.isNumeric(v) || "숫자만 쓰세요!",
+      ],
+    };
+  },
+  mounted() {},
+  methods: {
+    isNumeric(data) {
+      return !isNaN(data);
+    },
+    submitanswer() {
+      console.log;
+      if (this.number * this.number2 === parseInt(this.answer)) {
+        this.result = "딩동댕!";
+        this.number = Math.ceil(Math.random() * 9);
+        this.number2 = Math.ceil(Math.random() * 9);
+        this.answer = "";
+        this.$refs.answer.focus();
+      } else {
+        this.result = "땡!";
+        this.answer = "";
+        this.$refs.answer.focus();
+      }
+    },
+  },
+};
 </script>
 
 <style>
