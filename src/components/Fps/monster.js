fae99814e52279c16652412c5fae1e2f7603669c
steinjun0 <1142308@naver.com>
2021-08-04 18:49:12 +0900

    shuckshushuck 추가 및 클릭시 사라지는 코드 추가

--- src/components/Fps/monster.js
+++ src/components/Fps/monster.js
@@ -10,11 +10,27 @@ export class Monster {
       y,
     );
     this.size = 10;
+    this.gifIndex = 0
+    this.eliminated = false
+    this.velocity = 1/100
   }
   move(destinationX, destinationY){
-    console.log('destinationX',destinationX)
-    console.log('this.pos.x',this.pos.x)
-    console.log('(destinationX - this.pos.x)/5000',(destinationX - this.pos.x)/5000)
-    this.pos.setPos(this.pos.x + (destinationX - this.pos.x)/100, this.pos.y + (destinationY - this.pos.y)/100)
+    // console.log('destinationX',destinationX)
+    // console.log('this.pos.x',this.pos.x)
+    // console.log('(destinationX - this.pos.x)/5000',(destinationX - this.pos.x)/5000)
+    if(!this.eliminated){
+      this.pos.setPos(this.pos.x + (destinationX - this.pos.x)* this.velocity, this.pos.y + (destinationY - this.pos.y)* this.velocity)
+      this.size = 1*(this.pos.y)
+      if(this.size > 1000) this.size = 1000
+      if(this.size<0) this.size = 0
+      this.gifIndex += 1
+      if(this.gifIndex > 22) this.gifIndex = 0
+    }else{
+      this.size = 0
+    }
   }
+  isClicked(x, y){
+    return (this.pos.x - x)*(this.pos.x - x) + (this.pos.y - y)*(this.pos.y - y) <= (this.size / 8)*(this.size / 8)
+  }
+
 }
